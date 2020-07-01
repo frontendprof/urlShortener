1e3a047afc302d42157dbc160b40e0b997167a5a
AbdulMalik <abdumaliksharipov@gmail.com>
2020-07-01 12:38:04 +0500

    Creating post request route

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 
 const express = require("express");
+const mongoose=require("mongoose");
+const ShortUrl=require("./models/shortUrls")
 const app = express();
 
-app.set('view engine', 'ejs')
+
+mongoose.connect('mongodb://localhost/urlShortener',{
+    useNewUrlParser:true, useUnifiedTopology:true
+})
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended:false}))
 
 
-app.get('/',(req,res)=>{
-    res.render('index')
+app.get('/',async (req,res)=>{
+    const shortUrls=await ShortUrl.find()
+    res.render('index',shortUrls:shortUrls)
+})
+
+app.post("/shortUrls", async(req,res)=>{
+
+    await ShortUrl.create({full:req.body.fullUrl})
+
+  res.redirect('/')
+
 })
 
 app.listen(process.env.PORT ||5000);
